e5bd4ae9b4ad183b0fb95a9e55bb227cbbba3824
xeramirez <135636437+xeramirez@users.noreply.github.com>
2023-07-11 12:01:55 -0600

    V2
    Se agrega barra de busqueda

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,29 +1,45 @@
-import React from 'react';
-const SearchBar = () => {
-    return (
-      <div className="relative text-gray-600">
-        <input
-          type="search"
-          name="search"
-          placeholder="Search"
-          className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-        />
-        <button type="submit" className="absolute right-0 top-0 mt-3 mr-4">
-          <svg
-            className="h-4 w-4 fill-current"
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-          >
-            <path
-              fillRule="evenodd"
-              d="M13.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0z"
-              clipRule="evenodd"
-            ></path>
-          </svg>
-        </button>
-      </div>
+import React, { useState } from 'react';
+
+const SearchBar = ({ products }) => {
+  const [searchText, setSearchText] = useState('');
+  const [filteredProducts, setFilteredProducts] = useState([]);
+
+  const handleSearch = () => {
+    const filtered = products.filter((product) =>
+      product.title.toLowerCase().includes(searchText.toLowerCase())
     );
+    setFilteredProducts(filtered);
   };
-  
-  export default SearchBar;
-  
+
+  return (
+    <div className="relative text-gray-600">
+      <input
+        type="text"
+        name="search"
+        placeholder="Buscar productos"
+        value={searchText}
+        onChange={(e) => setSearchText(e.target.value)}
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
+      <button
+        type="submit"
+        className="absolute right-0 top-0 mt-3 mr-4"
+        onClick={handleSearch}
+      >
+        <svg
+          className="h-4 w-4 fill-current"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+        >
+          <path
+            fillRule="evenodd"
+            d="M13.707 12.293a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414l4-4a1 1 0 011.414 0z"
+            clipRule="evenodd"
+          ></path>
+        </svg>
+      </button>
+    </div>
+  );
+};
+
+export default SearchBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,44 @@
-import { useEffect, useState } from "react";
-import useAPI from "../hooks/useAPI";
-import ProductItem from "../components/ProductItem";
-import SearchBar from "../components/SearchBar";
+import React, { useEffect, useState } from 'react';
+import useAPI from '../hooks/useAPI';
+import ProductItem from '../components/ProductItem';
+import SearchBar from '../components/SearchBar';
+
 const Home = () => {
   const [products, setProducts] = useState([]);
+  const [filteredProducts, setFilteredProducts] = useState([]);
   const { getProducts } = useAPI();
 
   useEffect(() => {
     getProducts().then(setProducts);
   }, []);
 
+  const handleSearch = (searchTerm) => {
+    const filtered = products.filter((product) =>
+      product.title.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredProducts(filtered);
+  };
+
   return (
-    
     <section className="w-full min-h-[800px] flex justify-center">
       <div className="container flex flex-col items-center p-4">
-      <SearchBar/>
+        {/* <SearchBar onSearch={handleSearch}products= {products} /> */}
+        <input
+        type="text"
+        name="search"
+        placeholder="Buscar productos"
+        onChange={(e) => handleSearch(e.target.value)}
+        className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+      />
         <h1 className="font-bold text-3xl">Bienvenido a Fake Store</h1>
         <div className="flex flex-wrap gap-4">
-          {products.map((product) => (
-            <ProductItem product={product} />
-          ))}
+          {filteredProducts.length > 0
+            ? filteredProducts.map((product) => (
+                <ProductItem key={product.id} product={product} />
+              ))
+            : products.map((product) => (
+                <ProductItem key={product.id} product={product} />
+              ))}
         </div>
       </div>
     </section>
